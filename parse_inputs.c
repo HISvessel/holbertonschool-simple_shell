c9101e1d3e67feeb0ee905e6ce8fa26f3457663d
HISvessel <kevinsanchezdabest@yahoo.com>
2025-04-21 11:35:06 -0400

    adding changes to avoid freeing memory

---
+++ parse_inputs.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <strings.h>
+#include <stdlib.h>
+#include "shell.h"
+
+/**
+ * parse_inputs - this parses the inputs
+ * @args: strings to take
+ * @input: the input of string
+ */
+
+void parse_inputs(char *lineptr, char **args, const char *delim) /* added a new parameter delim*/
+{
+	char *token;
+	int i = 0;
+	
+	lineptr[strcspn(lineptr, "\n")] = 0; /*new updates from main*/
+	token = strtok(lineptr, delim);
+	while (token != NULL && i < MAX_ARGS - 1) /*added max counter for index i*/
+	{
+		args[i++] = token; /*pointer updated to the new token*/
+		token = strtok(NULL, delim);
+	}
+	args[i] = NULL; /* NULL is found */
+}
