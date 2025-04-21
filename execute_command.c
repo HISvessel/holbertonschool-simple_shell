543462a82f5e3c931a7321957f4f10f1f480c626
josniel <josnielramos@gmail.com>
2025-04-21 13:03:53 -0400

    adding an update

---
+++ execute_command.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * execute_command - Function to execute commands
+ * @args: Argument string in the function
+ *
+ * Return: Command to execute, 0 on succes, -1 on error
+ */
+
+int execute_command(char **args)
+{
+	if (fork_and_execute(args) == -1)
+	{
+		fprintf(stderr, "Failed to execute command\n");
+		return (-1);
+	}
+	return (0);
+}
