89d91b37ff41fb8ceadb22abbe325b4357855658
HISvessel <kevinsanchezdabest@yahoo.com>
2025-04-21 15:11:03 -0400

    changes to path and path token

--- find_executable.c
+++ find_executable.c
@@ -11,24 +11,34 @@
 char *find_executable(const char *command, const char *path)
 {
 	char *full_path = NULL;
-	char *path_token = strtok((char *)path, ":");
+	char *path_copy = strdup(path);
+	char *path_token = strtok(path_copy, ":");
 
+	if (!path_copy)
+	{
+		perror("strdup failed");
+		exit(EXIT_FAILURE);
+	}
 	while (path_token != NULL)
 	{
 		full_path = malloc(strlen(path_token) + strlen(command) + 2);
 		if (full_path == NULL)
 		{
 			perror("malloc failed");
+			free(path_copy);
 			exit(EXIT_FAILURE);
 		}
 
 		sprintf(full_path, "%s/%s", path_token, command);
 
 		if (access(full_path, X_OK) == 0)
+		{
+			free(path_copy);
 			return (full_path);
+		}
 		free(full_path);
 		path_token = strtok(NULL, ":");
-
 	}
+	free(path_copy);
 	return (NULL);
 }
